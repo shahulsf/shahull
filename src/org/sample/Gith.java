204a8ef960299bb440b34264abe814d07dc8e36c
shahulsf <shahulrockzz.h@gmail.com>
2023-01-12 05:10:21 -0800

    change

---
+++ src/org/sample/Gith.java
@@ -0,0 +1,10 @@
+package org.sample;
+
+public class Gith {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("github install completed");
+	}
+
+}
---
+++ src/org/sample/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author User
+ *
+ */
+package org.sample;
